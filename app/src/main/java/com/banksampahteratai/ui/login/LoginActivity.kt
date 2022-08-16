da7574cccf1ae856a23157fbc388b27d63521f71
Lasteinsa <mikueinsa@gmail.com>
2022-08-16 22:22:40 +0700

    playing with animation

--- app/src/main/java/com/banksampahteratai/ui/login/LoginActivity.kt
+++ app/src/main/java/com/banksampahteratai/ui/login/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.banksampahteratai.ui.login
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.banksampahteratai.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -13,5 +16,38 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        playAnimation()
+        setupAction()
+    }
+
+    private fun playAnimation() {
+        val imageLogin =
+            ObjectAnimator.ofFloat(binding.imgLogo, "alpha", 1f).setDuration(500)
+        val textWelcome =
+            ObjectAnimator.ofFloat(binding.tvLogin, "alpha", 1f).setDuration(500)
+        val usernameInput =
+            ObjectAnimator.ofFloat(binding.inpUsername, "alpha", 1f).setDuration(300)
+        val passwordInput =
+            ObjectAnimator.ofFloat(binding.inpPassword, "alpha", 1f).setDuration(300)
+        val loginButton =
+            ObjectAnimator.ofFloat(binding.btnLogin, "alpha", 1f).setDuration(300)
+
+        AnimatorSet().apply {
+            playSequentially(
+                imageLogin, textWelcome, usernameInput, passwordInput, loginButton
+            )
+            start()
+        }
+    }
+
+    private fun setupAction() {
+        binding.btnLogin.setOnClickListener {
+            tryLogin()
+        }
+    }
+
+    private fun tryLogin() {
+        binding.inpUsername
     }
 }
