a78dd3433d4b8c18aa71e845e2b3b23295d37376
Lasteinsa <mikueinsa@gmail.com>
2022-08-16 22:03:39 +0700

    splash and play with colors

---
+++ app/src/main/java/com/banksampahteratai/ui/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.banksampahteratai.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import com.banksampahteratai.R
+import com.banksampahteratai.databinding.ActivitySplashBinding
+import com.banksampahteratai.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
--- app/src/main/java/com/banksampahteratai/ui/login/LoginActivity.kt
+++ app/src/main/java/com/banksampahteratai/ui/login/LoginActivity.kt
@@ -12,5 +12,6 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.hide()
     }
 }
