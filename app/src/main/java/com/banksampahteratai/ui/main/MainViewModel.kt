996986219e36583b3e3b9452d72796c519ff675a
Lasteinsa <mikueinsa@gmail.com>
2022-11-09 15:28:12 +0700

    main layouting changes

--- app/src/main/java/com/banksampahteratai/ui/main/MainViewModel.kt
+++ app/src/main/java/com/banksampahteratai/ui/main/MainViewModel.kt
@@ -1,8 +1,24 @@
 package com.banksampahteratai.ui.main
 
+import android.app.Application
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.banksampahteratai.data.DataPreference
+import com.banksampahteratai.data.database.Nasabah
+import com.banksampahteratai.data.repository.NasabahRepository
 
-class MainViewModel(private val preference: DataPreference) : ViewModel() {
+class MainViewModel(application: Application, private val preference: DataPreference) : ViewModel() {
+    private val mNasabahRepository: NasabahRepository = NasabahRepository(application)
 
+    fun getAllNasabah(): LiveData<List<Nasabah>> = mNasabahRepository.getAllNasabah()
+
+    fun insert(nasabah: Nasabah) {
+        mNasabahRepository.insert(nasabah)
+    }
+    fun update(nasabah: Nasabah) {
+        mNasabahRepository.update(nasabah)
+    }
+    fun delete(nasabah: Nasabah) {
+        mNasabahRepository.delete(nasabah)
+    }
 }
